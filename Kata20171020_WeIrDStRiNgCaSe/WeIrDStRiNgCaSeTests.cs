17a4b9ac97532a60b1c1299b9f049f14cd28dccf
cash <cash@cashwu.com>
2017-10-20 22:56:45 +0800

    input ab

--- Kata20171020_WeIrDStRiNgCaSe/WeIrDStRiNgCaSeTests.cs
+++ Kata20171020_WeIrDStRiNgCaSe/WeIrDStRiNgCaSeTests.cs
@@ -13,13 +13,27 @@ namespace Kata20171020_WeIrDStRiNgCaSe
             var result = kata.ToWeirdCase("a");
             Assert.AreEqual("A", result);
         }
+
+
+        [TestMethod]
+        public void input_ab()
+        {
+            var kata = new Kata();
+            var result = kata.ToWeirdCase("ab");
+            Assert.AreEqual("Ab", result);
+        }
     }
 
     public class Kata
     {
         public string ToWeirdCase(string s)
         {
-            return "A";
+            if (s.Length == 1)
+            {
+                return s.ToUpper();
+            }
+
+            return $"{char.ToUpper(s[0])}{s[1]}";
         }
     }
 }
